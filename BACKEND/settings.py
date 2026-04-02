bdea1417cccc7376d3075d5e3bc63049d4c08ac8
paquinho89 <paquinho89@hotmail.com>
2026-04-02 16:59:14 +0200

    supabase_2

--- BACKEND/settings.py
+++ BACKEND/settings.py
@@ -122,7 +122,10 @@ DATABASES = {
         default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
-    )
+    ) if os.getenv("DATABASE_URL") else {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
